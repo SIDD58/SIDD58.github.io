4d13bdbae31463a4c4988cfd3dd13e056785c6a6
SIDD58 <siddharthsamber94@gmail.com>
2026-03-04 23:50:59 -0500

    a commit

--- portfolio-react/src/main.jsx
+++ portfolio-react/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './assets/css/main.css'
 import './assets/css/noscript.css'
-// import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
